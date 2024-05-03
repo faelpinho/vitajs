dae3978d88df070769ddb0d98e61abf816b65e44
Rafael Pinho <faelpinho@users.noreply.github.com>
2024-05-03 10:23:14 -0300

    WIP: simplify js execution.

--- src/main.c
+++ src/main.c
@@ -9,7 +9,7 @@
 #include <vitasdk.h>
 #include <stdbool.h>
 #include "common/debugScreen.h"
-#include "ath_env.h"
+#include "env.h"
 #include "graphics.h"
 
 int main(int argc, char *argv[])
@@ -20,21 +20,8 @@ int main(int argc, char *argv[])
     loadFont("app0:/assets/segoeui.ttf");
     printf("font loaded.");
 
-    const char *result = NULL;
+    const char *errMsg = NULL;
 
-    const char *list[8] = {
-        "EvalError",
-        "SyntaxError",
-        "TypeError",
-        "ReferenceError",
-        "RangeError",
-        "InternalError",
-        "URIError",
-        "AggregateError"};
-
-    int index = 0;
-
-    // sceCtrlSetSamplingMode(SCE_CTRL_MODE_ANALOG);
     uint32_t lastButtonState = 0;
     uint32_t currentButtonState = 0;
 
@@ -42,34 +29,24 @@ int main(int argc, char *argv[])
     uint32_t currentTime = lastTime;
     uint32_t deltaTime = 0;
 
-    printf("preparing to enter in loop...");
+    printf("VitaJS starting...");
+
+    do
+    {
+        errMsg = runScript("app0:/assets/main.js");
+    } while (errMsg != NULL);
+
+    start_drawing();
+
+    vitajs_error_screen(errMsg);
+
+    end_drawing();
+
+    printf("while start is not pressed...");
 
     while (ctrl.buttons != SCE_CTRL_START)
     {
-        sceCtrlPeekBufferPositive(0, &ctrl, 1);
-
-        currentButtonState = ctrl.buttons;
-        if (currentButtonState & SCE_CTRL_START && !(lastButtonState & SCE_CTRL_START))
-        {
-            break;
-        }
-        lastButtonState = currentButtonState;
-
-        currentTime = sceKernelGetProcessTimeLow();
-        deltaTime = currentTime - lastTime;
-
-        if (deltaTime >= (5 * 1000 * 1000))
-        {
-            index = (index + 1) % 8;
-            lastTime = currentTime;
-        }
-
-        start_drawing_and_clear();
-
-        athena_error_screen(list[index]);
-
-        end_drawing();
-        waitVblankStart();
+        return -1;
     }
 
     freeFont();
